4716a2215558c94a192eb2841a1a80a7196338eb
Galen Cuthbertson <g@galen.me>
2023-03-06 11:29:59 +1100

    Add lein-ancient to global user leiningen profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.27.3"]]}
+{:user {:plugins [[cider/cider-nrepl "0.27.3"]
+                  [lein-ancient  "1.0.0-RC3"]]}
  :repl-options {:init (require 'cljfmt.core)}
  }
